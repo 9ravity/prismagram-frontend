0b833e9e1e2c5c1ef6dcdd21373a8e8a68d7c8f1
9ravity <shegoback@naver.com>
2020-04-01 20:54:38 +0900

    apollo react setting

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Theme from "../Styles/Theme";
 import GlobalStyles from "../Styles/GlobalStyles";
 
+import { ThemeProvider } from "styled-components";
+import Router from "./Router";
+
 export default () => (
-  <>
+  <ThemeProvider theme={Theme}>
     <GlobalStyles />
-    hello
-  </>
+    <Router isLoggedIn={true} />>
+  </ThemeProvider>
 );
